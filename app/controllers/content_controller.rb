c29faa4b45db17ac6a6b685ff69973f70bec7963
Juzef <juzefwt@gmail.com>
2011-05-07 23:24:48 +0200

    Drobne porzadki na dobranoc

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -6,11 +6,18 @@ class ContentController < ApplicationController
       unless current_user.teacher.blank?
 	redirect_to :action => 'indexTeacher'
       end
+      @news = News.order('created_at DESC').limit(3)
+      @docs = Document.all
   end
   
   def indexTeacher
 #       if current_user.teacher.blank?
 # 	redirect_to :action => 'index'
 #       end
+    @classes = SchoolClass.joins(:teachers).where('teachers.id = ?', 1)
+    @plans = Plan.joins(:school_classes => :teachers)
+      .where('teachers.id = ?', 1)
+      .order('day DESC')
+      .limit(5)
   end
 end
