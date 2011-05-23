6a1ef6ddbe270d86359326e07650551d40b1a14e
Joker <Jokeroz@gmail.com>
2011-05-23 18:10:18 +0200

    asdf

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,29 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function hideDiv() {
+if(document.getElementById("flash_notice").style.display=="none") {
+   loadFlash();
+   document.getElementById("flash_notice").style.display="block";
+   document.getElementById("LEFT").style.display="none";
+   document.getElementById("RIGHT").style.display="none";
+ }
+else {
+   document.getElementById("flash_notice").style.display="none";
+   document.getElementById("LEFT").style.display="block";
+   document.getElementById("RIGHT").style.display="block";
+   }
+}
+
+function loadFlash() {
+    var flashvars = {};
+	var params = {};
+	var attributes = {};
+	swfobject.embedSWF("http://localhost/Cal1.swf", "flash_notice", "900", "780", "9.0.0","expressInstall.swf", flashvars, params, attributes);
+}
+
+$(document).ready(function() {
+  $('#my-link').click(function(event){
+    hideDiv();
+    event.preventDefault(); // Prevent link from following its href
+  });
+});
